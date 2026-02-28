73bf44a46e95d5f779f417ba385efda701aa6499
gouravanand662 <81240130+gouravanand662@users.noreply.github.com>
2026-03-01 00:36:36 +0530

    Refactor DataContext to use Business type

--- app/context/DataContext.tsx
+++ app/context/DataContext.tsx
@@ -2,6 +2,15 @@
 
 import { createContext, useContext } from "react";
 
-export const DataContext = createContext<any>(null);
+// export const DataContext = createContext<any>(null);
+type Business = {
+  slug: string;
+  name: string;
+  city: string;
+  phone: string;
+  description: string;
+};
+
+export const DataContext = createContext<Business | null>(null);
 
 export const useData = () => useContext(DataContext);
